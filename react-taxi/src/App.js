d6f883427cda7dadf21cd83755c2262fe232c72d
Pavel Sekishov <pasha-sveta@yandex.ru>
2021-09-04 11:39:38 +0300

    Add base

---
+++ react-taxi/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         Project React Taxi
+        </p>
+        <a
+          className="App-link"
+          href="https://psekishov.github.com.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Pavel Sekishov --- Green Taxi
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
